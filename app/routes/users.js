5796b63ce93d36b1fab74b29087453d7412c299d
BigD <109411790+BigD5658@users.noreply.github.com>
2024-01-31 00:32:37 +0800

    基礎mongo db 配置+jwt驗證

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,5 @@
-const jsonwebtoken = require('jsonwebtoken');
+//const jsonwebtoken = require('jsonwebtoken');
+const jwt = require("koa-jwt");
 const Router = require("koa-router");
 const router = new Router({ prefix: "/users" });
 const {
@@ -6,31 +7,33 @@ const {
   findById,
   create,
   update,
-  delete: del,login
-
+  delete: del,
+  login,
 } = require("../controllers/users");
-const {secert} = require('../config');
+const { secert } = require("../config");
 
-
+//--------------------------------------------------
 //認證的中間件,負責解析token取得實際內容,
 //先去header裡面會有key:authorization;  value:Bearer+" "+token
-const auth =async(ctx, next)=>{
-  //authorization='' 保險一點如果 沒有設置authorization那就變成空字串
-  const{authorization=''}=ctx.request.header;
-  //replace('Bearer','')要去除Bearer和空格 才能拿到純token
-  const token=authorization.replace('Bearer','');
-  
-  try{
-    //token,secret把這兩個值傳近來驗證是否有被串改過,有被改過或是傳空直近來都會回傳401error
-    const user = jsonwebtoken.verify(token,secret);
-    //為了確保後續的中間件能拿到驗證好的資料所以賦值到ctx.state.user
-    ctx.state.user = user;
+// const auth =async(ctx, next)=>{
+//   authorization='' 保險一點如果 沒有設置authorization那就變成空字串
+//   const{authorization=''}=ctx.request.header;
+//  replace('Bearer','')要去除Bearer和空格 才能拿到純token
+//   const token=authorization.replace('Bearer','');
 
-  }catch(err){
-    ctx.throw(401,err.message);
-  }
-  await next();
-}
+//   try{
+//     token,secret把這兩個值傳近來驗證是否有被串改過,有被改過或是傳空直近來都會回傳401error
+//     const user = jsonwebtoken.verify(token,secret);
+//     為了確保後續的中間件能拿到驗證好的資料所以賦值到ctx.state.user
+//     ctx.state.user = user;
+
+//   }catch(err){
+//     ctx.throw(401,err.message);
+//   }
+//   await next();
+// }
+const auth = jwt({ secret }); //用koa-jwt套件 取代上方的解析token等功能:auth
+//--------------------------------------------------
 
 // 建立一個暫時的db,重啟就會失效
 // const db = [{ name: "jack" }, { name: "Ada" }];
@@ -68,6 +71,6 @@ router.delete(
   //   db.splice(ctx.params.id * 1, 1); //其實可以寫成(0,1)=從第0項開始刪除1項
   //   ctx.status = 204; //回傳狀態碼
 );
-router.post('/login',login);
+router.post("/login", login);
 
 module.exports = router;
--- readme.md
+++ readme.md
@@ -1,3 +1,42 @@
+```mermaid
+sequenceDiagram
+
+    participant U as User
+    participant F as Frontend
+    participant B as Backend
+    participant DB as Database
+    alt  註冊register
+        U->>F: 登入頁面點選註冊register<BR>在register頁面填寫完data後送出表單
+        F->>B:使用post方法http://localhost:3000/users
+        B->>DB: 確認無重複資料後建立資料 
+        DB->>B:新建DATA到mongodb atlas,回傳
+        B->>F: 回傳JSON
+        F->>U: 成功註冊POP UP提示,導入回登入頁面
+end
+
+    U->>F: 用戶輸入帳號密碼
+    F->>B: 點選登入判斷帳號是否有存在資料庫<br>post方法http://localhost:3000/users/login
+    B->>DB: 去資料庫撈取對應的帳號進行比對
+    alt 登錄成功
+        DB->>B: 發送用戶數據,<br>返回JSON格式{name,password,user_id}
+        B->>F: 身份驗證成功,<br>透過user/login 的回傳api,將_id,auth_token<br>儲存在使用者前端的localStorage
+        F->>U: 前台返回登入成功,<br>並跳轉到首頁並<br>自動請求api http://localhost:3000/users/user_id<br>透過抓取json裡面的name來顯示用戶名稱
+    else 登錄失敗
+        DB->>B: 無效憑據
+        B->>F: 身份驗證失敗
+        F->>U: 顯示錯誤消息    
+
+
+
+end
+```
+
+這一個練習koa配置+mongo db的專案
+
 npm start代表在正式環境
+
 npm run dev代表在測試環境
+
 ctrl+c 離開環境
+
+
